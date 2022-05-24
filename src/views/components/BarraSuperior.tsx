24f8ab4c1e262a6fa8dcf6163991e9ed23fec84b
Nazareth Blazquez <blazquezjimenezn@iesmartinrivero.org>
2022-05-24 12:31:43 +0200

    Columnas añadidas y estilo actualizado

--- src/views/components/BarraSuperior.tsx
+++ src/views/components/BarraSuperior.tsx
@@ -1,5 +1,4 @@
 import { useContext } from "react";
-import { useNavigate } from "react-router-dom";
 import { Context } from "../../context/context";
 import { Boton, BarraHerramientas } from "../styles/styles";
 
@@ -9,13 +8,6 @@ const BarraSuperior = () => {
   return (
     <BarraHerramientas>
       <span>Hola, {localStorage.getItem("usuario")}</span>
-      <Boton
-        onClick={() => contexto.toggleDivCrear()}
-        style={{ fontSize: "18px", width: "150px" }}
-        className="btnAdd"
-      >
-        + Añadir tarea
-      </Boton>
       <Boton onClick={() => contexto.cerrarSesion()} className="cerrarSesion">
         Cerrar sesión
       </Boton>
--- src/views/kanban.view.tsx
+++ src/views/kanban.view.tsx
@@ -2,10 +2,7 @@ import { useContext, useState } from "react";
 import ModalFormCrear from "./components/ModalFormCrear";
 import ModalFormEditar from "./components/ModalFormEditar";
 import BarraSuperior from "./components/BarraSuperior";
-import {
-  ContenedorKanban,
-  Lista,
-} from "./styles/styles";
+import { Boton, ContenedorKanban, Lista } from "./styles/styles";
 import { Context } from "../context/context";
 import { Tarea } from "../domain/types/types";
 import TareaCard from "./components/TareaCard";
@@ -18,36 +15,85 @@ export const Kanban = () => {
     titulo: "",
     descripcion: "",
     estado: "",
-  }); 
-  
+  });
+
   return (
     <div>
       <BarraSuperior />
       <ContenedorKanban>
         <Lista>
           <h1 className="tituloLista">Nuevas Tareas</h1>
-          {contexto.tareasNuevas.map((tarea) => (
-            <TareaCard key={tarea.id} tarea={tarea} setDataModal={setDataModal} />
-          ))}
+          <div className="contenedor">
+            {contexto.tareasNuevas.map((tarea) => (
+              <TareaCard
+                key={tarea.id}
+                tarea={tarea}
+                setDataModal={setDataModal}
+              />
+            ))}
+          </div>
         </Lista>
         <Lista>
           <h1 className="tituloLista">En proceso</h1>
-          {contexto.tareasEnProceso.map((tarea) => (
-            <TareaCard key={tarea.id} tarea={tarea} setDataModal={setDataModal} />
-          ))}
+          <div className="contenedor">
+            {contexto.tareasEnProceso.map((tarea) => (
+              <TareaCard
+                key={tarea.id}
+                tarea={tarea}
+                setDataModal={setDataModal}
+              />
+            ))}
+          </div>
         </Lista>
         <Lista>
           <h1 className="tituloLista">Terminadas</h1>
-          {contexto.tareasTerminadas.map((tarea) => (
-            <TareaCard key={tarea.id} tarea={tarea} setDataModal={setDataModal} />
-          ))}
+          <div className="contenedor">
+            {contexto.tareasTerminadas.map((tarea) => (
+              <TareaCard
+                key={tarea.id}
+                tarea={tarea}
+                setDataModal={setDataModal}
+              />
+            ))}
+          </div>
+        </Lista>
+        <Lista>
+          <h1 className="tituloLista">Verificadas</h1>
+          <div className="contenedor">
+            {contexto.tareasVerificadas.map((tarea) => (
+              <TareaCard
+                key={tarea.id}
+                tarea={tarea}
+                setDataModal={setDataModal}
+              />
+            ))}
+          </div>
+        </Lista>
+        <Lista>
+          <h1 className="tituloLista">Bloqueadas</h1>
+          <div className="contenedor">
+            {contexto.tareasBloqueadas.map((tarea) => (
+              <TareaCard
+                key={tarea.id}
+                tarea={tarea}
+                setDataModal={setDataModal}
+              />
+            ))}
+          </div>
         </Lista>
         <div id="modalCrear" style={{ display: "none" }}>
           <ModalFormCrear />
         </div>
         <div id="modalEditar" style={{ display: "none" }}>
-          <ModalFormEditar dataModal={dataModal} setDataModal={setDataModal}/>
+          <ModalFormEditar dataModal={dataModal} setDataModal={setDataModal} />
         </div>
+        <Boton
+          onClick={() => contexto.toggleDivCrear()}
+          style={{ fontSize: "18px", width: "150px" }}
+          className="btnAdd"
+        >
+          + Añadir tarea
+        </Boton>
       </ContenedorKanban>
     </div>
   );
