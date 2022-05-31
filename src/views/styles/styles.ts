ba81e8708572f3aa8ee680f641802d9cf6410f30
Nazareth Blazquez <blazquezjimenezn@iesmartinrivero.org>
2022-05-31 10:32:41 +0200

    Overflow de fases eliminado

--- src/views/styles/styles.ts
+++ src/views/styles/styles.ts
@@ -212,29 +212,7 @@ export const Lista = styled.div`
     color: white;
   }
 
-  .contenedor {
-    overflow-y: auto;
-    max-height: 700px;
-    /* width */
-    ::-webkit-scrollbar {
-      width: 10px;
-    }
 
-    /* Track */
-    ::-webkit-scrollbar-track {
-      background: #f1f1f1;
-    }
-
-    /* Handle */
-    ::-webkit-scrollbar-thumb {
-      background: #888;
-    }
-
-    /* Handle on hover */
-    ::-webkit-scrollbar-thumb:hover {
-      background: #555;
-    }
-  }
 
   @media screen and (max-width: 767px) {
     margin: 0 auto;
