274f1277e761760bb11a4b5dc45b18000a1edcad
Michellen <michellemasias00@gmail.com>
2022-06-07 12:46:53 +0200

    todo ok

--- src/domain/api/conexionApiTiempo.tsx
+++ src/domain/api/conexionApiTiempo.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { WeatherData } from "../types/tiempo.dtos";
+
 let datosUbicacion: GeolocationCoordinates = {
   latitude: undefined as unknown as number,
   longitude: undefined as unknown as number,
@@ -27,17 +28,12 @@ const instance = axios.create({
     lang: "es",
   },
 });
-
-
-const recogerDatos = () => { 
-  if (datosUbicacion) {
-    return instance.get<WeatherData>("/", {
-      params: {
-        lat: datosUbicacion.latitude,
-        lon: datosUbicacion.longitude,
-      },
-    });
-  }
-
+const recogerDatos = () => {
+  return instance.get<WeatherData>("/", {
+    params: {
+      lat: datosUbicacion.latitude,
+      lon: datosUbicacion.longitude,
+    },
+  });
 };
 export const tiempoService = { recogerDatos };
