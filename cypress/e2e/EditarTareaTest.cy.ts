2244f2517664891f0b5a091ae9514d48d12112ec
Michellen <michellemasias00@gmail.com>
2022-06-09 08:24:01 +0200

    modificiaciones en el test de cypress editar tarea

--- cypress/e2e/EditarTareaTest.cy.ts
+++ cypress/e2e/EditarTareaTest.cy.ts
@@ -1,9 +1,12 @@
 /// <reference types="cypress" />
 describe("empty spec", () => {
-  it("passes", () => {
+  it("Open the edit modal", () => {
     localStorage.setItem("usuario", "sdasdd");
     cy.visit("http://localhost:3000/organizador");
     cy.get('[data-testid="1buttoneditar"]').click();
+  });
+
+  it('Check if the form contains the values', () => {
     cy.get('[data-testid="input-titulo"]').should("have.value", "Mi tarea 1");
     cy.get('[data-testid="input-descripcion"]').should(
       "have.value",
@@ -17,15 +20,19 @@ describe("empty spec", () => {
       "have.value",
       "Subtarea 2"
     );
+  })
+
+  it('Changes the values of the form and saves them', () => {
     cy.get('[data-testid="input-titulo"]').clear().type("Tarea editada");
     cy.get('[data-testid="input-descripcion"]').clear().type("Descripción editada");
-      
     cy.get('[data-testid="sButton0"]').click();
     cy.get('[type="submit"]').click();
+  });
 
+  it('check if the values have change', () => {
     cy.get('[data-testid="input-titulo"]').should('not.exist')
     cy.get('[data-testid="titulo-tarea-1"]').should("exist");
     cy.get('#2').should('be.checked')
-    cy.get('[data-testid="porcentaje1"]').should('have.text','Subtareas: 100%');
-  });
+    cy.get('[data-testid="porcentaje1"]').should('have.text', 'Subtareas: 100%');
+  })
 });
