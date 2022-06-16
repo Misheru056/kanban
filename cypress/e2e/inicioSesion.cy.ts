32823be6e5796e8e3142f3adfae81e662ebab8ba
Nazareth Blazquez <blazquezjimenezn@iesmartinrivero.org>
2022-06-16 14:52:00 +0200

    Cypress mejorado

--- cypress/e2e/inicioSesion.cy.ts
+++ cypress/e2e/inicioSesion.cy.ts
@@ -1,9 +1,35 @@
 /// <reference types="cypress" />
 
-it("Successful login", () => {
-    cy.visit('http://localhost:3000');
-    cy.get('#username').type('Ana');
-    cy.get('#passwd').type('Pru3ba.???');
-    cy.get('[type=submit]').click();
-    cy.get('.error').should('not.exist');
+describe("Login", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+  });
+
+  it("Successful login", () => {
+    cy.login("Ana", "Pru3ba?????");
+    cy.fixture("login").then((login) => {
+      cy.get(login.error).should("not.exist");
+    });
+  });
+
+  it("Login with no user", () => {
+    cy.login("", "Pru3ba?????");
+    cy.fixture("login").then((login) => {
+      cy.get(login.error).should("exist");
+    });
+  });
+
+  it("Login with no password", () => {
+    cy.login("Ana", "");
+    cy.fixture("login").then((login) => {
+      cy.get(login.error).should("exist");
+    });
+  });
+
+  it("Login with not valid password pattern", () => {
+    cy.login("Ana", "Passw");
+    cy.fixture("login").then((login) => {
+      cy.get(login.error).should("contain", "Formato de contraseña inválido");
+    });
+  });
 });
